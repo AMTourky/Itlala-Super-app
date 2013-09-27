c2100b24559e41f9022c5b11650eae64d5cb3867
AMTourky <AMTourky@Abdelhameeds-Mac-mini.local>
2013-09-27 18:00:07 +0200

    Initial Commit

---
+++ Itlala-Super-app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Itlala-Super-app
+//
+//  Created by Abdelhameed Tourky on 9/27/13.
+//  Copyright (c) 2013 AMTourky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
